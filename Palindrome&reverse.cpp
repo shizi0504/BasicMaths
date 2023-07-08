da3cb4522dbd308eae4d765d35588d43eecafcae
shizi0504 <81694117+shizi0504@users.noreply.github.com>
2023-07-08 17:43:23 +0530

    Create Palindrome&reverse.cpp

---
+++ Palindrome&reverse.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std ;
+int reverNum(int n)
+{
+    int ans = 0 ;
+    while(n != 0)
+    {
+        int lst = n%10 ;
+        if((ans > INT_MAX/10) || (ans < INT_MIN/10))
+            return 0 ;
+        ans = (ans*10)+lst ;
+        n = n/10 ;
+    }
+    return ans ;
+}
+int main()
+{
+    int n ;
+    cout<<"Enter the number: ";
+    cin>>n ;
+    cout<<"The reversed number is: "<<reverNum(n)<<endl ;
+    
+    int revnum = reverNum(n);
+    if(n == revnum)
+        cout<<"Palindrome"<<endl ;
+    else
+        cout<<"Not palindrome" ;
+        
+    return 0;
+    
+}
